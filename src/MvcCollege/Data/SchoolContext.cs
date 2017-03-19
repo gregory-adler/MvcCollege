21a118e638d55363c9b4e5140c465a86f1410462
Gregory Adler <greg7239@gmail.com>
2017-03-19 19:38:38 -0400

    Added entities and db context

---
+++ src/MvcCollege/Data/SchoolContext.cs
@@ -0,0 +1,16 @@
+﻿using MvcCollege.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MvcCollege.Data
+{
+    public class SchoolContext:DbContext
+    {
+        public SchoolContext(DbContextOptions<SchoolContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Student> Students { get; set; }
+    }
+}
