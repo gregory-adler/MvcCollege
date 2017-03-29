d404061965be528172177f6c0d00f24fe6363c3c
Gregory Adler <greg7239@gmail.com>
2017-03-29 18:11:47 -0400

    added migration of new data features and coursemigration entity

--- src/MvcCollege/Migrations/20170329214124_newmigration.Designer.cs
+++ src/MvcCollege/Migrations/20170329214124_newmigration.Designer.cs
@@ -8,9 +8,10 @@ using MvcCollege.Data;
 namespace MvcCollege.Migrations
 {
     [DbContext(typeof(SchoolContext))]
-    partial class SchoolContextModelSnapshot : ModelSnapshot
+    [Migration("20170329214124_newmigration")]
+    partial class newmigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.1.1")
