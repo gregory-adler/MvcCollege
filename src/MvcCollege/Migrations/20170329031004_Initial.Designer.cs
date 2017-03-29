e71c1a57d101f33a46a591b704a457bf1b1e779f
Gregory Adler <greg7239@gmail.com>
2017-03-28 23:19:31 -0400

    changed display names

--- src/MvcCollege/Migrations/20170329031004_Initial.Designer.cs
+++ src/MvcCollege/Migrations/20170329031004_Initial.Designer.cs
@@ -8,9 +8,10 @@ using MvcCollege.Data;
 namespace MvcCollege.Migrations
 {
     [DbContext(typeof(SchoolContext))]
-    partial class SchoolContextModelSnapshot : ModelSnapshot
+    [Migration("20170329031004_Initial")]
+    partial class Initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.1.1")
--- src/MvcCollege/Migrations/20170329031401_InitialMigrations.Designer.cs
+++ src/MvcCollege/Migrations/20170329031401_InitialMigrations.Designer.cs
@@ -8,9 +8,10 @@ using MvcCollege.Data;
 namespace MvcCollege.Migrations
 {
     [DbContext(typeof(SchoolContext))]
-    partial class SchoolContextModelSnapshot : ModelSnapshot
+    [Migration("20170329031401_InitialMigrations")]
+    partial class InitialMigrations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.1.1")
