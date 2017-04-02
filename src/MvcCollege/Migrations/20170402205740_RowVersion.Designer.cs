b322407728339b7bd23a9c5d22f6d850543823cf
Gregory Adler <greg7239@gmail.com>
2017-04-02 17:01:58 -0400

    added Departments controller and index view

--- src/MvcCollege/Migrations/20170402205740_RowVersion.Designer.cs
+++ src/MvcCollege/Migrations/20170402205740_RowVersion.Designer.cs
@@ -8,9 +8,10 @@ using MvcCollege.Data;
 namespace MvcCollege.Migrations
 {
     [DbContext(typeof(SchoolContext))]
-    partial class SchoolContextModelSnapshot : ModelSnapshot
+    [Migration("20170402205740_RowVersion")]
+    partial class RowVersion
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.1.1")
